6afaf911d908bc550cf7413ec472dfb9000a5cac
NInad joshi <93592190+Ninadjos@users.noreply.github.com>
2025-06-11 15:45:32 +0530

    Revert "Branch update"

--- src/mitre_mapper.py
+++
@@ -1,7 +0,0 @@
-def mitre_mapping(log: str) -> str:
-    if "powershell" in log.lower():
-        return "T1059: Command and Scripting Interpreter"
-    elif "login failed" in log.lower():
-        return "T1110: Brute Force"
-    else:
-        return "Unknown Technique"
