5199a6b258b4a7a488b8a951614627e60008af99
NInad joshi <93592190+Ninadjos@users.noreply.github.com>
2025-06-11 15:38:35 +0530

    Delete src directory

--- src/triage.py
+++
@@ -1,7 +0,0 @@
-def triage_alert(log: str) -> str:
-    if "powershell" in log.lower():
-        return "High"
-    elif "login failed" in log.lower():
-        return "Medium"
-    else:
-        return "Low"
